9fc4e99b3ab98b036961fd13f754d212c4cf2570
Thinnd <thinnd.work@gmail.com>
2023-09-29 14:12:49 +0700

    feat: first code

---
+++ constants/constants.py
@@ -0,0 +1,3 @@
+SRC_BLOCK = "C:/Users/Thinnd/Desktop/projects/snake-hunting-game/assets/images/block.png"
+
+SRC_DIAMOND = "C:/Users/Thinnd/Desktop/projects/snake-hunting-game/assets/images/diamond.png"
